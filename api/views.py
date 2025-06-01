6e26ca9c49bfd109902ac19881732c49e344d5e0
rbc33 <ricardobenthem@gmail.com>
2025-06-01 02:33:57 +0200

    md to html ?

--- api/views.py
+++ api/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import get_object_or_404, render
+import markdown
 from .models import Posts
+from django.utils.safestring import mark_safe
 
 
 # Create your views here.
@@ -10,6 +12,10 @@ def index(request):
 
 def post_detail(request, post_id):
     post = get_object_or_404(Posts, id=post_id)
+    post.content_html = mark_safe(
+        markdown.markdown(post.content, extensions=["extra", "codehilite"])
+    )
+
     if request.htmx:
         return render(request, "api/partials/post_detail.html", {"post": post})
     return render(request, "api/post.html", {"post": post})
