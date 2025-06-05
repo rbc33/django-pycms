6178bbe3b1ce824aa877c4d3b69b360c42e97957
rbc33 <ricardobenthem@gmail.com>
2025-06-06 00:18:00 +0200

    fakemd post iframe

--- fakemd.py
+++ fakemd.py
@@ -4,18 +4,38 @@ import requests
 
 
 def post_fake(PORT):
-    fake = Faker()
-    fake.add_provider(MarkdownPostProvider)
-    fake_post = fake.post(size="medium")  # available sizes: 'small', 'medium', 'large'
+    # fake = Faker()
+    # fake.add_provider(MarkdownPostProvider)
+    # fake_post = fake.post(size="medium")  # available sizes: 'small', 'medium', 'large'
+    fake_post = """# My Game of Life
+
+        <iframe 
+        src="https://rbc33.github.io/html-life/" 
+        width="820" 
+        height="620" 
+        frameborder="0"
+        style="border: 2px solid #4a5568; border-radius: 8px;">
+        </iframe>
+
+        ## Controles:
+        - **P**: Pausar/Reanudar
+        - **R**: Patrón aleatorio
+        - **+/-**: Cambiar velocidad"""
+    # post = {
+    #     "title": fake.text(max_nb_chars=20),
+    #     "excerpt": fake.text(max_nb_chars=200),
+    #     "content": fake_post,
+    # }
     post = {
-        "title": fake.text(max_nb_chars=20),
-        "excerpt": fake.text(max_nb_chars=200),
+        "title": "the game of life",
+        "excerpt": """lets see it the game works!! click to discover!""",
         "content": fake_post,
     }
     print(post)
 
     make_post = requests.post(
         f"http://localhost:{PORT}/posts",
+        # f"http://192.168.0.100:{PORT}/posts",
         json=post,
         headers={"Content-type": "application/json"},
     )
@@ -26,5 +46,5 @@ def post_fake(PORT):
 
 if __name__ == "__main__":
     PORT = 8000
-    for _ in range(5):
+    for _ in range(1):
         post_fake(PORT)
--- pycms/settings.py
+++ pycms/settings.py
@@ -77,26 +77,26 @@ WSGI_APPLICATION = "pycms.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.mysql",
-#         "NAME": "gocms",
-#         "HOST": "192.168.0.100",
-#         "PORT": "33060",
-#         "USER": "root",
-#         "PASSWORD": "secret",
-#     }
-# }
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "gocms",
-        "USER": "root",
-        "PASSWORD": "root",
-        "HOST": "127.0.0.1",
+        "HOST": "192.168.0.100",
         "PORT": "33060",
+        "USER": "root",
+        "PASSWORD": "secret",
     }
 }
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.mysql",
+#         "NAME": "gocms",
+#         "USER": "root",
+#         "PASSWORD": "root",
+#         "HOST": "127.0.0.1",
+#         "PORT": "33060",
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
